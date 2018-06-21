09bf689a09e6fd02c8040cb4852f1a84a745a43e
Irina Tari <irinatari95@gmail.com>
2018-06-21 18:07:36 +0300

    table view cells UI

--- AnimalsFirst/App Sections/History/Xibs/HistoryTableViewCell.swift
+++ AnimalsFirst/App Sections/History/Xibs/HistoryTableViewCell.swift
@@ -9,16 +9,7 @@
 import UIKit
 
 class HistoryTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
+    @IBOutlet weak var appointmentLabel: UILabel!
+    @IBOutlet weak var dropOffLabel: UILabel!
+    @IBOutlet weak var pickUpLabel: UILabel!
 }
